9b1bbb2db81b83e781d92ce565ff38247ab99bbe
tonydtran <anthony.tranvan@gmail.com>
2020-03-31 14:39:07 +0200

    Add menu buildsettings base code

--- src/components/Builder/index.js
+++ src/components/Builder/index.js
@@ -1,12 +1,15 @@
 import React from 'react'
-import { /*useRouteMatch,*/ useHistory } from 'react-router-dom'
+import styled from 'styled-components'
+import { /*useRouteMatch,*/ /*useHistory*/ } from 'react-router-dom'
 import Button from 'react-bootstrap/Button'
+
+import BuildSettings from './menus/BuildSettings'
 
 const Builder = () => {
   // const routeMatch = useRouteMatch()
   // console.log(routeMatch)
   // TODO: Use routeMatch to fetch existing build
-  const history = useHistory()
+  // const history = useHistory()
 
 
   return (
@@ -16,9 +19,21 @@ const Builder = () => {
           <i className="fas fa-chevron-left fa-sm" /> My builds
         </Button>
       </div>
-      <h1 className="mb-5">New build</h1>
+      <div className="d-flex align-items-center mb-5">
+        <h1 className="d-inline-block text-truncate">New build</h1>
+        <I className="fas fa-cog fa-lg text-success ml-2" />
+      </div>
+      <BuildSettings />
     </>
   )
 }
+
+const I = styled.i`
+  &:hover, &:active {
+    transform: scale(1.3);
+    cursor: pointer;
+    transition: transform 300ms;
+  }
+`
 
 export default Builder
---
+++ src/components/Builder/menus/BuildSettings.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const BuildSettings = () => {
+  return (
+    <div>
+      MENU
+    </div>
+  )
+}
+
+export default BuildSettings
