46d84597123707bf01efba0e12a9ca5cdb6ad3cb
Anthony Tran Van <anthony.tranvan@gmail.com>
2020-04-09 21:03:49 +0200

    Add settings and reset password desktop view

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -7,6 +7,7 @@ import Spinner from 'react-bootstrap/Spinner'
 import { FirebaseContext } from '../Firebase'
 import { withAuthorization } from '../Session'
 import ViewHeader from '../shared/layouts/ViewHeader'
+import Viewport from '../shared/layouts/Viewport'
 
 const Settings = () => {
   const firebase = useContext(FirebaseContext)
@@ -22,51 +23,62 @@ const Settings = () => {
   }, [firebase])
 
   return (
-    <>
-      <ViewHeader>
-        <h1>Settings</h1>
-      </ViewHeader>
-      <Card bg="dark">
-        <Card.Header>
-          <h4 className="mb-0">My account</h4>
-        </Card.Header>
-        <Card.Body>
-          <div className="mb-4">
-            <div className="d-flex justify-content-between mb-2">
-              <p className="font-weight-bold mb-0">Email address</p>
-              <Link to="/settings/update_email">
-                <i className="fas fa-pencil-alt fa-sm" /> Edit
+    <Viewport>
+      {viewport => (
+        <>
+          <ViewHeader>
+            <h1>Settings</h1>
+          </ViewHeader>
+          <Card bg="dark">
+            <Card.Header>
+              <h4 className="mb-0">My account</h4>
+            </Card.Header>
+            <Card.Body>
+              <div className="mb-4">
+                <div className="d-flex justify-content-between mb-2">
+                  <p className="font-weight-bold mb-0">Email address</p>
+                  <Link to="/settings/update_email">
+                    <i className="fas fa-pencil-alt fa-sm" /> Edit
               </Link>
-            </div>
-            <p>
-              {
-                userEmail
-                  ? userEmail
-                  : <Spinner as="span" animation="border" variant="light" size="sm" />
-              }
-            </p>
-          </div>
-          <div className="">
-            <div className="d-flex justify-content-between mb-2">
-              <p className="font-weight-bold mb-0">Password</p>
-              <Link to="/settings/update_password">
-                <i className="fas fa-pencil-alt fa-sm" /> Edit
+                </div>
+                <p>
+                  {
+                    userEmail
+                      ? userEmail
+                      : <Spinner as="span" animation="border" variant="light" size="sm" />
+                  }
+                </p>
+              </div>
+              <div className="">
+                <div className="d-flex justify-content-between mb-2">
+                  <p className="font-weight-bold mb-0">Password</p>
+                  <Link to="/settings/update_password">
+                    <i className="fas fa-pencil-alt fa-sm" /> Edit
               </Link>
-            </div>
+                </div>
+              </div>
+            </Card.Body>
+          </Card>
+          <div
+            className={viewport !== 'desktop'
+              ? 'mt-5 pl-3 pr-3'
+              : 'd-flex justify-content-center mt-5'
+            }
+          >
+            <Button
+              className="d-flex align-items-baseline"
+              variant="outline-danger"
+              block={viewport !== 'desktop'}
+              size="sm"
+              onClick={firebase.doSignOut}
+            >
+              <i className="fas fa-sign-out-alt fa-sm fa-rotate-180 mr-2" />
+              Disconnect
+            </Button>
           </div>
-        </Card.Body>
-      </Card>
-      <div className="mt-5 pl-3 pr-3">
-        <Button
-          variant="outline-danger"
-          block
-          size="sm"
-          onClick={firebase.doSignOut}
-        >
-          <i className="fas fa-sign-out-alt fa-sm fa-rotate-180" /> Disconnect
-      </Button>
-      </div>
-    </>
+        </>
+      )}
+    </Viewport>
   )
 }
 
--- src/components/auth/ResetPassword.js
+++ src/components/auth/ResetPassword.js
@@ -10,6 +10,7 @@ import { StoreContext } from '../Store'
 import { FirebaseContext } from '../Firebase'
 import { AuthUserContext } from '../Session'
 import ViewHeader from '../shared/layouts/ViewHeader'
+import Viewport from '../shared/layouts/Viewport'
 
 const firebaseErrorHandler = error => {
   // TODO: Check once write/read change if errors change as well
@@ -45,63 +46,69 @@ const ResetPassword = ({ history }) => {
   }
 
   return (
-    <>
-        <ViewHeader>
-          {
-            authUser && (
-              <div className="mt-2 mb-2">
-                <Link to="/settings/update_password">
-                  <i className="fas fa-chevron-left fa-sm" /> Update password
+    <Viewport>
+      {viewport => (
+        <>
+          <ViewHeader>
+            {
+              authUser && (
+                <div className="mt-2 mb-2">
+                  <Link to="/settings/update_password">
+                    <i className="fas fa-chevron-left fa-sm" /> Update password
               </Link>
-              </div>
-            )
-          }
-          <h1>Reset password</h1>
-        </ViewHeader>
-      <Card bg="dark">
-        <Card.Body>
-          <Form noValidate onSubmit={handleSubmit(onSubmit)}>
-            <Form.Group controlId="email">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control
-                name="email"
-                type="email"
-                placeholder="typical.yasuo@rito.com"
-                isInvalid={errors.email}
-                ref={register({ required: 'Required.' })}
-              />
-              {errors.email && (
-                <Form.Control.Feedback type="invalid">{errors.email.message}</Form.Control.Feedback>
-              )}
-              <Form.Text>We will send you a link to reset your password</Form.Text>
-            </Form.Group>
-            <Button
-              className="mt-4"
-              variant="primary"
-              type="submit"
-              size="lg"
-              block
-              disabled={Object.keys(errors).length > 0 || isLoading}
-            >
+                </div>
+              )
+            }
+            <h1>Reset password</h1>
+          </ViewHeader>
+          <Card bg="dark">
+            <Card.Body>
+              <Form noValidate onSubmit={handleSubmit(onSubmit)}>
+                <Form.Group controlId="email">
+                  <Form.Label>Email address</Form.Label>
+                  <Form.Control
+                    name="email"
+                    type="email"
+                    placeholder="typical.yasuo@rito.com"
+                    isInvalid={errors.email}
+                    ref={register({ required: 'Required.' })}
+                  />
+                  {errors.email && (
+                    <Form.Control.Feedback type="invalid">{errors.email.message}</Form.Control.Feedback>
+                  )}
+                  <Form.Text>We will send you a link to reset your password</Form.Text>
+                </Form.Group>
+                <div className="d-flex justify-content-center mt-5">
+                  <Button
+                    variant="primary"
+                    type="submit"
+                    size="lg"
+                    block={viewport !== 'desktop'}
+                    disabled={Object.keys(errors).length > 0 || isLoading}
+                    style={viewport === 'desktop' ? { minWidth: '162px' } : undefined}
+                  >
+                    {
+                      isLoading
+                        ? <Spinner as="span" animation="border" variant="light" />
+                        : 'Send reset link'
+                    }
+                  </Button>
+                </div>
+              </Form>
               {
-                isLoading
-                  ? <Spinner as="span" animation="border" variant="light" />
-                  : 'Send reset link'
+                !authUser && (
+                  <div className="text-center mt-4">
+                    <p className="mb-0">
+                      Don't have an account? <Link to="/signup">Sign up here!</Link>
+                    </p>
+                  </div>
+                )
               }
-            </Button>
-          </Form>
-          {
-            !authUser && (
-              <div className="text-center mt-4">
-                <p className="mb-0">
-                  Don't have an account? <Link to="/signup">Sign up here!</Link>
-                </p>
-              </div>
-            )
-          }
-        </Card.Body>
-      </Card>
-    </>
+            </Card.Body>
+          </Card>
+        </>
+      )}
+    </Viewport>
   )
 }
 
